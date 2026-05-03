30b4962820324f13673af76c5f98656f47b8d067
Vinicius Santos <vfssantos@outlook.com>
2026-05-03 13:09:11 -0300

    fixing type checks

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,7 +1,16 @@
-/// <reference lib="deno.worker" />
-
 import type { ResponseMsg, WorkerMsg } from "../shared/types.ts";
 import { handleWorkerMessage } from "./handler.ts";
+
+type WorkerGlobal = {
+  postMessage(message: ResponseMsg): void;
+  addEventListener(
+    type: "message",
+    listener: (ev: MessageEvent<WorkerMsg>) => void | Promise<void>,
+  ): void;
+  close(): void;
+};
+
+const self = globalThis as unknown as WorkerGlobal;
 
 // Simple postMessage wrapper
 const post = (msg: ResponseMsg) => {
